c7d9c1b417876e7e187825079526be8292b36302
Nama' Salameh <nama.salameh@gmail.com>
2023-10-26 01:03:07 +0300

    Use the components in the app.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
-
+import style from "./App.module.css";
+import NoteKeeperHeader from "./components/NoteKeeperHeader/NoteKeeperHeader.jsx";
+import NoteForm from "./components/NoteForm/NoteForm";
+import NoteList from "./components/NoteList/NoteList";
+import {NotesProvider} from "./components/NoteContext";
+import { useState } from "react";
 function App() {
+  const [searchText, setSearchText] = useState("");
+
   return (
-    <div className="App">
-      
+    <div className={style.appContainer}>
+      <NotesProvider>
+        <NoteKeeperHeader onSearchChange={setSearchText}/>
+        <NoteForm />
+        <NoteList searchText={searchText}/>
+      </NotesProvider>
     </div>
   );
 }
